6ea32a0ba6385fb2cfc8b534ef24016a1ba99491
Hugo Prado Figueiredo <hugo4566@gmail.com>
2014-05-06 20:40:19 -0300

    Adicionado Main java

---
+++ Main.java
@@ -0,0 +1,35 @@
+import parser.*;
+import lexer.*;
+import node.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		long start_time, stop_time;
+
+		if(args.length < 1){
+			System.out.println("Passe algum arquivo, como parametro, para testar");
+		}else{
+
+	        try {
+	            start_time = System.currentTimeMillis();// create lexer
+	            Lexer lexer = new Lexer (new PushbackReader(new BufferedReader(new FileReader(args[0])), 1024));
+//	            Parser parser = new Parser(lexer);
+//	            Start ast = parser.parse(); 
+//	            ast.apply(new SemanticAnalyser());  
+//	            ast.apply(new ClassGenerator());
+	        }
+	        catch (Exception e) {
+	            System.out.println(e);
+	        }
+		}
+	}
+
+}
